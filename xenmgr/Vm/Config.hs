2c8c3c2087a6ae250759a7fe3e8065929ec0f4a8
Chris Rogers <rogersc@ainfosec.com>
2017-02-06 15:05:34 -0500

    fix cdrom write
    Signed-off-by: Chris Rogers <rogersc@ainfosec.com>

--- xenmgr/Vm/Config.hs
+++ xenmgr/Vm/Config.hs
@@ -715,9 +715,9 @@ miscSpecs cfg = do
               -- no cdrom
               (False, _)    -> ""
               -- full access to cdrom
-              (True, True)  -> printf "file=%s:%s,media=cdrom,if=atapi-pt,format=atapi-pt-fmt,readonly=%s" atapiType bsg_str "off"
+              (True, True)  -> printf "file=%s:%s,media=disk,if=atapi-pt,format=atapi-pt-fmt" atapiType bsg_str
               -- readonly access to cdrom
-              (True, False) -> printf "file=%s:%s,media=cdrom,if=atapi-pt,format=atapi-pt-fmt,readonly=%s" atapiType bsg_str "on"
+              (True, False) -> printf "file=%s:%s,media=disk,if=atapi-pt,format=atapi-pt-fmt,readonly" atapiType bsg_str
         atapiType = if (vmcfgStubdom cfg) then "atapi-pt-v4v" else "atapi-pt-local"
 
     let empty = pure []
